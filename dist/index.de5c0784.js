fd8a50a51259de2ed4a7d0ba3f3b02bce80d22cf
Noah <103532278+noahanutta@users.noreply.github.com>
2024-08-04 19:55:42 -0700

    Updated

---
+++ dist/index.de5c0784.js
@@ -0,0 +1,19 @@
+let menuSwitch = document.querySelector(".menuButton");
+let menu = document.querySelector(".menu");
+let menuOpen = false;
+let menuCloseTimer;
+menuSwitch.addEventListener("click", ()=>{
+    if (menuOpen) {
+        menu.classList.remove("activo");
+        menuSwitch.classList.remove("activo");
+        menuSwitch.innerHTML = "Menu";
+        menuOpen = false;
+    } else {
+        menu.classList.add("activo");
+        menuSwitch.innerHTML = "Close";
+        menuSwitch.classList.add("activo");
+        menuOpen = true;
+    }
+});
+
+//# sourceMappingURL=index.de5c0784.js.map
